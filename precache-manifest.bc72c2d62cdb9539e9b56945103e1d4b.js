7be829af5bfd76ce969ee13e41797112f8221fe9
TuanAnh <anh.nqt1600@gmail.com>
2022-12-30 00:45:48 +0700

    Updates

---
+++ precache-manifest.bc72c2d62cdb9539e9b56945103e1d4b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4203d716f311594da528",
+    "url": "/htmlcssjs/static/js/main.4203d716.chunk.js"
+  },
+  {
+    "revision": "9b270a3d63eef61492ad",
+    "url": "/htmlcssjs/static/js/1.9b270a3d.chunk.js"
+  },
+  {
+    "revision": "4911258749371f9ccd56",
+    "url": "/htmlcssjs/static/js/runtime~main.49112587.js"
+  },
+  {
+    "revision": "c43e3d2e6cc66ec100782f923062293f",
+    "url": "/htmlcssjs/index.html"
+  }
+];
